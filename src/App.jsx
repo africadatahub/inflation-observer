7baba2ba84fdf72247f0fc332b49d5f136d7161a
Di M <meerkdir@gmail.com>
2023-05-23 14:16:56 +0200

    Fixed header issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { Header } from './components/Header';
-
 import { Home } from './pages/Home';
 import { Country } from './pages/Country';
 
@@ -10,7 +8,6 @@ import { locationToUrl, urlToLocation } from './utils/func.js';
 function App() {
   return (
     <div>
-        <Header />
         { document.location.search.includes('country=') ? <Country /> : <Home /> }
     </div>
   );
